26193e03d7b9d17ee3eca17fdf0141c66146f43c
SetHet <36393893+SetHet@users.noreply.github.com>
2019-02-27 23:54:35 -0300

    math+

---
+++ Alacran1/Assets/Scripts/Utilities/UtilitiesMath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class UtilitiesMath
+    {
+        public static float SenAbs2(float x, float long_wave = 1f)
+        {
+            if (long_wave == 0) return 0f;
+            return Mathf.Abs(Mathf.Sin(Mathf.PI*x/long_wave));
+        }
+
+        public static float RemapFloat(float value, float preRangeMin = 0f, float preRangeMax = 1f, float posRangeMin = 0f, float posRangeMax = 1f)
+        {
+            float percent = 0f;
+            float Dif = preRangeMax - preRangeMin;
+            if (Dif != 0) percent = value - preRangeMin / Dif;
+            else return posRangeMin;
+
+            Dif = posRangeMax - posRangeMin;
+            return (Dif * percent) + posRangeMin;
+        }
+    }
+}
