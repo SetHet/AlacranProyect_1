d52251928118ddae0edc3639c2f9bdd3efc56c16
SetHet <36393893+SetHet@users.noreply.github.com>
2019-03-08 19:58:35 -0300

    Update UtilitiesMath.cs

--- Alacran1/Assets/Scripts/Utilities/UtilitiesMath.cs
+++ Alacran1/Assets/Scripts/Utilities/UtilitiesMath.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Utilities
 {
-    public static class UtilitiesMath
+    public class UtilitiesMath
     {
         public static float SenAbs2(float x, float long_wave = 1f)
         {
@@ -22,5 +22,38 @@ namespace Utilities
             Dif = posRangeMax - posRangeMin;
             return (Dif * percent) + posRangeMin;
         }
+
+        [System.Serializable]
+        public class AlgoritmoForPoint
+        {
+            public List<Vector2> points;
+            public TypeConnection typeConnection = TypeConnection.lineal;
+            public enum TypeConnection
+            {
+                lineal, sin
+            }
+
+            public void AddPoint(float x, float y)
+            {
+                AddPoint(new Vector2(x, y));
+            }
+
+            public void AddPoint(Vector2 point)
+            {
+                points.Add(point);
+                points.Sort(); //falta decir que se ordene segun la x
+            }
+            
+            public Vector2[] GetPoints()
+            {
+                return points.ToArray();
+            }
+
+            public float GetValue(float x)
+            {
+                return 0;
+            }
+        }
     }
+    
 }
