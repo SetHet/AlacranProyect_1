60bfcee1188f13415c419dba48dddfeb9ca458c3
SetHet <36393893+SetHet@users.noreply.github.com>
2019-03-15 23:18:32 -0300

    Create ClearWindows.cmd

---
+++ Tools/ClearWindows.cmd
@@ -0,0 +1,22 @@
+erase /s *.csproj
+erase /s *.unityproj
+erase /s *.sln
+erase /s *.suo
+erase /s *.tmp
+erase /s *.user
+erase /s *.userprefs
+erase /s *.pidb
+erase /s *.booproj
+erase /s *.svd
+erase /s *.pdb
+erase /s *.mdb
+erase /s *.opendb
+erase /s *.VC.db
+erase /s *.pidb.meta
+erase /s *.pdb.meta
+erase /s *.mdb.meta
+erase /s *.apk
+erase /s *.unitypackage
+
+echo "Completado"
+pause
