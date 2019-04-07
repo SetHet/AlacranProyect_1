97d886e49599b21904ef4d84a6e3ab8dd21d70da
SetHet <36393893+SetHet@users.noreply.github.com>
2019-04-07 01:35:28 -0400

    inv

---
+++ Alacran1/Assets/Scripts/ItemConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GamePlay
+{
+    public static class ItemConfig
+    {
+        public static string joke_mensaje = "Kane";
+        
+        public enum Ammo
+        {
+            Pistol,
+            Shotgun,
+            Ligera
+        }
+    }
+
+    #if UNITY_EDITOR
+    public class ItemConfig_Editor : EditorWindow
+    {
+        [MenuItem ("Window/Gameplay/Item")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(ItemConfig_Editor));
+        }
+
+        private void OnGUI()
+        {
+            ItemConfig.joke_mensaje = GUI.TextArea(new Rect(0, 0, 100, 20), ItemConfig.joke_mensaje);
+        }
+        
+    }
+    #endif
+}
---
+++ Alacran1/Assets/Scripts/Player/PlayerBag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBag : MonoBehaviour
+{
+    public Items items;
+    [System.Serializable]
+    public class Items
+    {
+        [SerializeField] protected short _botoquines = 0;
+        [SerializeField] protected short _max_botiquines = 3;
+        public short botiquines { get { return _botoquines; } }
+        public short maxBotiquines { get { return _max_botiquines; } }
+        public ushort AddBotiquin(ushort cantidad)
+        {
+            
+
+            return 0;
+        }
+
+
+        [SerializeField] protected int _placasArmadura = 0;
+
+    }
+}
