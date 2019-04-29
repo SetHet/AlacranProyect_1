9429aa8614ada4746fb665e9d842e5b911e9def6
SetHet <36393893+SetHet@users.noreply.github.com>
2019-04-29 12:34:38 -0400

    trap behaviour

---
+++ Alacran1/Assets/Scripts/Interaccion/EJ/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : TrapBehaviuor
+{
+    [SerializeField] private Vector3 pos = new Vector3(0, 2, 0);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!DetectPlayer(other.gameObject)) return;
+
+        other.gameObject.GetComponent<CharacterController>().enabled = false;
+        other.transform.position = pos;
+        other.gameObject.GetComponent<CharacterController>().enabled = true;
+    }
+}
---
+++ Alacran1/Assets/Scripts/Interaccion/TrapBehaviuor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TrapBehaviuor : MonoBehaviour
+{
+    #region Variables
+    public string tagPlayer = "Player";
+    #endregion
+
+    protected virtual bool DetectPlayer(GameObject obj)
+    {
+        return obj.tag == tagPlayer;
+    }
+    
+}
