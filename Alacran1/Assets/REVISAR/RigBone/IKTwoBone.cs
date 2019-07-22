80e9f2e0d3e9a54b29f35b93c74ec3b2cb826af5
SetHet <36393893+SetHet@users.noreply.github.com>
2019-07-21 23:54:47 -0400

    new rig

---
+++ Alacran1/Assets/REVISAR/RigBone/IKTwoBone.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RigBone
+{
+
+    //[ExecuteInEditMode]
+    public class IKTwoBone : MonoBehaviour
+    {
+
+        #region Configuracion
+        [Header("Bones")]
+        public Transform bone_pre;
+        public Transform bone_mid;
+        public Transform bone_pos;
+        [Header("Rigs")]
+        public Transform effector;
+        public Transform codo;
+        [Header("Advanced")]
+        public bool EnableRotationExtern = true;
+        #endregion
+
+        #region MonoBehaviour
+        private void Update()
+        {
+            IKTwo();
+        }
+        #endregion
+
+        #region Funciones
+        private void IKTwo()
+        {
+            Transform A = bone_pre;
+            Transform B = bone_mid;
+            Transform C = bone_pos;
+            Transform E = effector;
+            Transform F = codo;
+
+            Vector3 ab = Formulas.GetDirPos(A, B);
+            Vector3 bc = Formulas.GetDirPos(B, C);
+            Vector3 ac = Formulas.GetDirPos(A, C);
+            Vector3 axis = -Vector3.Cross(ab, bc).normalized;
+
+            if (effector != null)
+            {
+                Vector3 ae = Formulas.GetDirPos(A, E);
+                Vector3 be = Formulas.GetDirPos(B, E);
+
+
+                float abcAngle = Formulas.TriangleAngle(ae.magnitude, ab, bc);
+                Formulas.InRange(ref abcAngle, 1, 179);
+                float angle = Vector3.SignedAngle(-ab, bc, axis);
+                float angle_dif = abcAngle - angle;
+                B.Rotate(axis, angle_dif, Space.World);
+
+                ac = Formulas.GetDirPos(A, C);
+                Formulas.newDireccion(A, ac, ae);
+            }
+
+            if (codo != null)
+            {
+                Vector3 af = Formulas.GetDirPos(A, F);
+                ac = Formulas.GetDirPos(A, C);
+                ab = Formulas.GetDirPos(A, B);
+                Vector3 med_ac = (A.transform.position + C.transform.position) / 2f;
+
+                Vector3 c_axis = ac.normalized;
+                Vector3 mac_f = F.transform.position - med_ac;
+                Vector3 mac_b = B.transform.position - med_ac;
+                mac_f = Vector3.ProjectOnPlane(mac_f, c_axis);
+                mac_b = Vector3.ProjectOnPlane(mac_b, c_axis);
+
+                float angle = Vector3.SignedAngle(mac_b, mac_f, c_axis);
+                A.Rotate(c_axis, angle, Space.World);
+            }
+
+            if (EnableRotationExtern)
+            {
+                C.transform.rotation = E.transform.rotation;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Alacran1/Assets/REVISAR/RigBone/RigBoneFormulas.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RigBone
+{
+    public class Formulas
+    {
+        public static float TriangleAngle(float aLen, Vector3 v1, Vector3 v2)
+        {
+            float aLen1 = v1.magnitude;
+            float aLen2 = v2.magnitude;
+            aLen = Mathf.Clamp(aLen, 0, aLen1 + aLen2);
+            float c = Mathf.Clamp((aLen1 * aLen1 + aLen2 * aLen2 - aLen * aLen) / ((aLen1 * aLen2) * 2.0f), -1.0f, 1.0f);
+            return Mathf.Acos(c) * Mathf.Rad2Deg;
+        }
+
+        public static void Rotate(Transform obj, Quaternion rotation)
+        {
+            obj.rotation = rotation * obj.rotation;
+        }
+
+        public static void newDireccion(Transform t, Vector3 da, Vector3 db)
+        {
+            Vector3 axis = Vector3.Cross(da, db);
+            float angle = Vector3.SignedAngle(da, db, axis);
+            t.Rotate(axis, angle, Space.World);
+        }
+
+        public static void InRange(ref float value, float min, float max)
+        {
+            if (value < min) value = min;
+            else if (value > max) value = max;
+        }
+
+        public static Vector3 GetDirPos(Transform a, Transform b)
+        {
+            return b.transform.position - a.transform.position;
+        }
+
+        public static void DrawBone(Transform a, Transform b)
+        {
+            Debug.DrawLine(a.transform.position, b.transform.position, Color.red);
+        }
+
+        public static void DrawAxis(Transform point, Vector3 v)
+        {
+            Debug.DrawLine(point.transform.position, point.transform.position + v, Color.blue);
+        }
+
+    }
+}
