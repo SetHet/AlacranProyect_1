dd63214eae9e99e08d63a8538f1858de9c313e90
SetHet <36393893+SetHet@users.noreply.github.com>
2019-07-30 01:22:52 -0400

    animacion diana

---
+++ Alacran1/Assets/CAULDRON/Scripts/Dianas/Diana.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diana : MonoBehaviour
+{
+    public Animator animator;
+
+    public bool startPos = false;
+
+    public float cooldown = 3f;
+
+    float timecooldown = 0;
+
+    bool up;
+
+    private void Awake()
+    {
+        if (animator == null)
+        {
+            enabled = false;
+            return;
+        }
+        up = startPos;
+        ActualizarAnimator();
+    }
+
+    private void Update()
+    {
+        if (up) return;
+        if (timecooldown < Time.time)
+        {
+            up = true;
+            ActualizarAnimator();
+        }
+    }
+
+    private void Down()
+    {
+        up = false;
+        ActualizarAnimator();
+        timecooldown = Time.time + cooldown;
+    }
+
+    void ActualizarAnimator()
+    {
+        if (!enabled) return;
+        animator.SetBool("up", up);
+    }
+
+}
