f2ff8299ee968d2e5d731ea1643e6ad152bafd18
SetHet <36393893+SetHet@users.noreply.github.com>
2019-08-29 17:55:21 -0400

    sonido ametralladora

---
+++ Alacran1/Assets/CAULDRON/Scripts/Player/Armas/Weapon_Ametralladora.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Ametralladora : MonoBehaviour
+{
+    #region 
+    public AudioSource audioFire;
+    public AudioSource audioReload;
+    public AudioClip audioClipReaload_A;
+    public AudioClip audioClipReaload_B;
+    #endregion
+
+
+    #region Metodos De Animacion
+    public void Fire_Ametralladora()
+    {
+        audioFire.Play();
+    }
+    
+    public void Reload_Ametralladora_1()
+    {
+        audioReload.clip = audioClipReaload_A;
+        audioReload.Play();
+    }
+    public void Reload_Ametralladora_2()
+    {
+        audioReload.clip = audioClipReaload_B;
+        audioReload.Play((ulong)0.6f);
+    }
+    
+    #endregion
+}
