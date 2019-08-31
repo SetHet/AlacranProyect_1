1609deb0f28d767f63fd521e11f2544efb6dd1b1
SetHet <36393893+SetHet@users.noreply.github.com>
2019-08-30 20:00:35 -0400

    Cruceta Ametralladora

---
+++ Alacran1/Assets/CAULDRON/Scripts/Player/Armas/CruzArma.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CruzArma : MonoBehaviour
+{
+    #region Variables
+    public Camera camera;
+    public Transform weaponForward;
+    public RectTransform uiPoint;
+    public float distance = 50f;
+    public LayerMask layers = -1;
+    #endregion
+
+    #region Methods Behaviour
+    private void Update()
+    {
+        if (Close())
+        {
+            Debug.Log("Faltan parametros");
+            return;
+        }
+
+        Vector3 inicio = weaponForward.position;
+        Vector3 direccion = weaponForward.forward;
+        RaycastHit hit;
+        if (Physics.Raycast(inicio, direccion, out hit, distance, layers, QueryTriggerInteraction.Ignore))
+        {
+            MoverPuntero(hit.point);
+        }
+        else
+        {
+            MoverPuntero(inicio + direccion * distance);
+        }
+    }
+    #endregion
+
+    #region Methods
+    bool Close() => camera == null || weaponForward == null || uiPoint == null;
+    void MoverPuntero(Vector3 posicion)
+    {
+         uiPoint.SetPositionAndRotation(camera.WorldToScreenPoint(posicion), Quaternion.identity);
+    }
+    #endregion
+}
